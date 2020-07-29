597d0412a742b1bcdad7f927b23dd1c565fc4ab3
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-30 00:21:45 +0530

    minor changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function NavBar() {
     return (
+        <div className="main-navbar">
         <nav className="navbar sticky-top navbar-expand-md navbar-dark">
-            <a className="navbar-brand ml-lg-5 ml-md-4 ml-sm-3" href="/home">
+            <a className="navbar-brand ml-lg-5 ml-md-4 ml-sm-3" href="/">
                 <h4 className="brand-name">Brydo </h4>
                 <p className = 'tagline'>A bangarus product</p>
             </a>
@@ -29,12 +30,12 @@ function NavBar() {
                     aria-expanded="false"   aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button> */}
-                <div class="navbar-toggler" >
-                    <a class="navbar-toggle-icon" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="navbar-toggler" >
+                    <a className="navbar-toggle-icon" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span className="navbar-toggler-icon"></span>
                     </a>
 
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink" style={{height: window.innerHeight - 64}}>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink" style={{height: window.innerHeight - 64}}>
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
                                 <a className="nav-link" href="/ride">Ride <span className="sr-only">(current)</span></a>
@@ -52,6 +53,7 @@ function NavBar() {
             
             
         </nav>
+        </div>
     )
 }
 
