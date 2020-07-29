492f5b7be2751c327a4e6ed6b025ef8f79fdf306
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-30 00:21:11 +0530

    feat: created notfound comp

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound() {
+    return (
+        <div>
+            Not Found
+            <br />
+            <a href="/">click to return to homepage</a>
+        </div>
+    )
+}
+
+export default NotFound
