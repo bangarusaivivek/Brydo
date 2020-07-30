f124bbd3be555b77926b331da1ff3ca44242f27d
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-30 23:00:00 +0530

    created modal for menu item

---
+++ client/src/components/menu.modal.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+
+function MenuModal() {
+    const [isLogin, setLogin] = useState(false);
+    const show = isLogin ? "dropdown-menu show" : "dropdown-menu"
+    const symbol = isLogin ? "X" : "=";
+    return (
+        <div className="navbar-toggler" onClick={() => setLogin(prevState => !prevState)} >
+            <span className="navbar-toggler-icon"></span>
+            {/* {symbol} */}
+            <div className={`dropdown-menu pl-lg-5 pl-md-4 pl-sm-3 pr-lg-5 pr-md-4 pr-sm-3 ${show} `}  style={{height: window.innerHeight - 64}}>
+                <div className="menu-modal">
+                    
+                        <a href="" className="menu-container">Ride</a>
+                        <a href="" className="menu-container">Drive</a>
+                        <a href="" className="menu-container">Partner</a>
+                        <a href="" className="menu-container">About Us</a>
+                        <a href="" className="menu-container">Help</a>
+                    
+                </div>   
+            </div>
+        </div>
+    )
+    
+        // <div className="navbar-toggler" >
+        //             <a className="navbar-toggle-icon" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    
+        //             </a>
+
+        //             <div className="dropdown-menu" aria-labelledby="dropdownMenuLink" style={{height: window.innerHeight - 64}}>
+        //                 <ul className="navbar-nav mr-auto">
+        //                     <li className="nav-item active">
+        //                         <a className="nav-link" href="/ride">Ride <span className="sr-only">(current)</span></a>
+        //                     </li>
+        //                     <li className="nav-item">
+        //                         <a className="nav-link" href="/drive">Drive</a>
+        //                     </li>
+        //                     <li className="nav-item">
+        //                         <a className="nav-link" href="/partner">Partner</a>
+        //                     </li>
+        //                 </ul>
+        //             </div>
+        //         </div>
+    
+}
+
+export default MenuModal
