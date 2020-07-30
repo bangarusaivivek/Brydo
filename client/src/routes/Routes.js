4f238f5dbfb834e6aa98b317233e737023f12bc9
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-30 23:02:32 +0530

    minor change

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -4,12 +4,14 @@ import Home from '../components/Home';
 import NavBar from '../components/NavBar';
 import Ride from '../components/Ride';
 import NotFound from '../components/NotFound';
+import RiderLogin from '../components/signin/rider.login';
 
 function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/ride" exact component={Ride} />
+            <Route path = "/riderlogin" exact component={RiderLogin} />
             <Route path="*" exact component = {NotFound} />
         </Switch>
     )
