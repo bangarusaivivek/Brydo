1a6b34a657bcbf0a69d2c7d89ec4f26f90417125
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-30 23:00:22 +0530

    refactored code for dropdown

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SigninModal from './signin/Signin.modal';
 import SignUpModal from './signup/signup.modal';
+import MenuModal from './menu.modal';
 
 function NavBar() {
     return (
-        <div className="main-navbar">
-        <nav className="navbar sticky-top navbar-expand-md navbar-dark">
+        <div className="main-navbar sticky-top">
+        <nav className="navbar navbar-expand-md navbar-dark">
             <a className="navbar-brand ml-lg-5 ml-md-4 ml-sm-3" href="/">
                 <h4 className="brand-name">Brydo </h4>
                 <p className = 'tagline'>A bangarus product</p>
@@ -32,25 +33,7 @@ function NavBar() {
                     aria-expanded="false"   aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button> */}
-                <div className="navbar-toggler" >
-                    <a className="navbar-toggle-icon" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span className="navbar-toggler-icon"></span>
-                    </a>
-
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink" style={{height: window.innerHeight - 64}}>
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="/ride">Ride <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/drive">Drive</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/partner">Partner</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <MenuModal />
             </div>
             
             
