d5fa3dc53e8b7f683239a703c9f91afd1589f86c
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-31 00:31:09 +0530

    feat: add Link to redirect to page

---
+++ client/src/components/signin/driver.login.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
+
+function DriverLogin() {
+    const height = window.offsetHeight;
+    // console.log(height)
+    const [number,setNumber] = useState("");
+    function handleClick(e){
+        e.preventDefault()
+        console.log(number)
+        setNumber("")
+        return
+    }
+
+    return (
+        <div className="rider-login-page"  >
+            <div className="rider-login-form-container">
+                <div className="rider-login-form-header">
+                    <Link to="/"> Homepage</Link>
+                    <Link to="/riderlogin">Login as Rider</Link>
+                </div>
+                <div className= "rider-login-form-body">
+                    <div className="rider-login-form-bodyContainer">
+
+                        <div className="rider-form-logo">logo</div>
+                        <form className="rider-form-data" onSubmit={handleClick}>
+                        
+                            <div style={{height: 100}}>Enter your phone number</div>
+                            <div className="rider-login-number">
+                            
+                                <div className="country-code">+91</div>
+                                <input className="rider-phonenumber" type="tel" placeholder="Phone number" value= {number}  onChange={e => setNumber(e.target.value)} />
+                                    
+                            </div>
+                            <button style={{width:100}}>Next</button>
+                        
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className="rider-login-container"></div>
+        </div>
+    )
+}
+
+export default DriverLogin
