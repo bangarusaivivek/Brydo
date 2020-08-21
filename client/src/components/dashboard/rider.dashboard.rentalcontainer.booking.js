dc6d9fbc9da524bccd3d8c90aa25c42c97567a0c
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-08-22 02:54:14 +0530

    made changes for md

--- client/src/components/dashboard/rider.dashboard.rentalcontainer.booking.js
+++ client/src/components/dashboard/rider.dashboard.rentalcontainer.booking.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
 import RiderNavbar from './rider.navbar';
-import { FormControl, InputLabel, Select, MenuItem  } from '@material-ui/core';
+import { FormControl, InputLabel, Select, MenuItem, FormControlLabel, Checkbox, TextField  } from '@material-ui/core';
 
 
 function RentalBooking() {
     const [select,setSelect] = useState();
     const [packages, setPackage] = useState("none");
+    const [pickup,setPickup] = useState(false);
+    const [bookingButtonSM,setBookingButtomSM] = useState("");
 
-    const handleClick = (e) =>{
+    const [promocodeValue,setPromocodeValue] = useState("");
+
+    const [selectXL,setSelectXL] = useState();
+    const [packagesXL, setPackageXL] = useState("none");
+    const [pickupXL,setPickupXL] = useState(false);
+
+    const handleClickSM = (e) =>{
+        
         e.preventDefault();
         e.stopPropagation();
         if(select){
@@ -15,23 +24,62 @@ function RentalBooking() {
         }
             
         if(e.target.id === "1" || e.target.id === "2" || e.target.id === "3" || e.target.id === "4" ){
-            
+            console.log(" inside target")
             document.getElementById(e.target.id).classList.add("tenureselect");
             setSelect(e.target.id);
             setPackage("none");
+            setBookingButtomSM("show")
         }
       
         if(e.target.name === "packageselect"){
-            setPackage(e.target.value)
-        }
 
+            setPackage(e.target.value)
+            setSelect()
+            setBookingButtomSM("show")
+        }
+    }
+    const handleClickXL = (e) =>{
+        console.log("XL")
+        e.preventDefault();
+        e.stopPropagation();
+        if(selectXL){
+            document.getElementById(selectXL).classList.remove("tenureselect");
+        }
+            
+        if(e.target.id === "XL1" || e.target.id === "XL2" || e.target.id === "XL3" || e.target.id === "XL4" ){
+            console.log(" inside target")
+            document.getElementById(e.target.id).classList.add("tenureselect");
+            setSelectXL(e.target.id);
+            setPackageXL("none");
+        }
+      
+        if(e.target.name === "packageselect"){
+
+            setPackageXL(e.target.value);
+            setSelectXL();
+        }
     }
 
+    const promoCodeSubmit = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(promocodeValue)
+        setPromocodeValue("");
+    }
+    useEffect(() => {
+        if(!select && packages === "none"){
+            setBookingButtomSM("");
+        }
+
+    },[packages,select])
+    console.log(bookingButtonSM,packages,select)
+    console.log(!select)
     return (
         <>
             <RiderNavbar />
             <div className = "rider-rental-booking-container ml-lg-5 ml-md-4 ml-sm-3 mr-lg-5 mr-md-4 mr-sm-3">
                 <div className="rider-rental-booking-data-container">
+                    <div className = "rider-rental-booking-back-button"></div>
                     <div className="rental-booking-img">
                         <img src="https://imgd.aeplcdn.com/0x0/n/cw/ec/26742/swift-exterior-right-front-three-quarter-117655.gif" style={
                                 {maxWidth:"100%",maxHeight:"100%",minWidth:"100%",minHeight:"100%"}} />
@@ -78,25 +126,25 @@ function RentalBooking() {
                             <div className = "rental-package-tenure-name">tenure</div>
                             <div className = "rental-package-tenure-row">
                                 <div className = "rental-package-tenure-row-block">
-                                    <div className = "rental-package-tenure-row-inner-block" id="1" onClick={handleClick}>
+                                    <div className = "rental-package-tenure-row-inner-block" id="1" onClick={handleClickSM}>
                                         <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>6</div>
                                         <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
                                     </div>
                                 </div>
                                 <div className = "rental-package-tenure-row-block">
-                                    <div className = "rental-package-tenure-row-inner-block" id="2" onClick={handleClick}>
+                                    <div className = "rental-package-tenure-row-inner-block" id="2" onClick={handleClickSM}>
                                         <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>12</div>
                                         <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
                                     </div>
                                 </div>
                                 <div className = "rental-package-tenure-row-block">
-                                    <div className = "rental-package-tenure-row-inner-block" id="3" onClick={handleClick}>
+                                    <div className = "rental-package-tenure-row-inner-block" id="3" onClick={handleClickSM}>
                                         <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>18</div>
                                         <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
                                     </div>
                                 </div>
                                 <div className = "rental-package-tenure-row-block">
-                                    <div className = "rental-package-tenure-row-inner-block" id="4" onClick={handleClick}>
+                                    <div className = "rental-package-tenure-row-inner-block" id="4" onClick={handleClickSM}>
                                         <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>24</div>
                                         <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
                                     </div>
@@ -105,7 +153,7 @@ function RentalBooking() {
                             </div>
                         </div>
                         <div className ="rental-booking-package-list">
-                            <div style={{minWidth:"40%",maxWidth:"40%",display:"flex",justifyContent:"center",alignItems:"center",fontSize:"18px",fontWeight:"600px"}}>select package</div>
+                            <div style={{minWidth:"50%",maxWidth:"50%",display:"flex",justifyContent:"center",alignItems:"center",fontSize:"18px",fontWeight:"600px"}}>select package</div>
                             <div className = "rental-booking-package-selectform">
                                 <FormControl style={{minWidth:"140px",maxWidth:"140px"}}>
                                     <Select
@@ -113,7 +161,7 @@ function RentalBooking() {
                                         id="demo-simple-select"
                                         name = "packageselect"
                                         value={packages}
-                                        onChange={handleClick}
+                                        onChange={handleClickSM}
                                     >
                                         <MenuItem value={"none"}>none</MenuItem>
                                         <MenuItem value={"3 days"}>3 days</MenuItem>
@@ -124,9 +172,138 @@ function RentalBooking() {
                                 </FormControl>
                             </div>
                         </div>
+                        <div className = "rental-booking-pricing-block">
+                            <div className = "rental-booking-price-tag">
+                                <span className="rental-booking-price-tag-block" style={{minWidth:"20px",fontSize:"26px",fontWeight:"400",justifyContent:"center",alignItems:"center"}}>₹ </span>
+                                <span className="rental-booking-price-tag-block" style={{fontSize:"28px",fontWeight:"600",justifyContent:"center",alignItems:"center"}}>1,000 </span>
+                                <span className="rental-booking-price-tag-block" style={{fontSize:"12px",fontWeight:"400",alignItems:"flex-end",padding:"7px 3px"}}>/12hr </span>
+                            </div>
+                            <div style={{minHeight:"16px",maxHeight:"16px",fontSize:"12px",textAlign:"left",paddingLeft:"5px",fontWeight:"400"}}>
+                                (Inclusive of taxes)
+                            </div>
+                            <div style={{minHeight:"24px",maxHeight:"24px",fontSize:"15px",textAlign:"left",paddingLeft:"5px",fontWeight:"600"}}>+ 2rs per km</div>
+                            <div></div>
+                        </div>
                     </div>
-                    <div className = "rental-booking-pickup"></div>
+                    <div className = "rental-booking-pickup">
+                        <div style={{minWidth:"50%",maxWidth:"50%",display:"flex",justifyContent:"center",alignItems:"center"}}>
+                        <FormControlLabel
+                            control={<Checkbox checked={pickup} onChange={e => setPickup(e.target.checked)} name="checkedA" />}
+                            label="self-pickup"
+                        />
+                        </div>
+                        <div style={{minWidth:"50%",maxWidth:"50%",display:"flex",flexDirection:"column",justifyContent:"center",}}>
+                            <div style={{fontSize:"12px",fontWeight:"400"}}>place of delivery</div>
+                            <div style={{fontSize:"13px",fontWeight:"500"}}>at your doorstep</div>
+                        </div>
+                    </div>
                     <div className = "rental-booking-vehicle-related"></div>
+                    <div className = {`rental-booking-confirm-button-small ${bookingButtonSM}` }>
+                        <div className="rental-booking-confirm-button-small-block">confirm booking</div>
+                    </div>
+                </div>
+
+                <div className = "rental-booking-package-large">
+                    <div className = "rental-booking-package-large-block"> 
+                        <div className = "rental-package-tenure">
+                            <div className = "rental-package-tenure-name">tenure</div>
+                            <div className = "rental-package-tenure-row">
+                                    <div className = "rental-package-tenure-row-block">
+                                        <div className = "rental-package-tenure-row-inner-block" id="XL1" onClick={handleClickXL}>
+                                            <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>6</div>
+                                            <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
+                                        </div>
+                                    </div>
+                                    <div className = "rental-package-tenure-row-block">
+                                        <div className = "rental-package-tenure-row-inner-block" id="XL2" onClick={handleClickXL}>
+                                            <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>12</div>
+                                            <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
+                                        </div>
+                                    </div>
+                                    <div className = "rental-package-tenure-row-block">
+                                        <div className = "rental-package-tenure-row-inner-block" id="XL3" onClick={handleClickXL}>
+                                            <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>18</div>
+                                            <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
+                                        </div>
+                                    </div>
+                                    <div className = "rental-package-tenure-row-block">
+                                        <div className = "rental-package-tenure-row-inner-block" id="XL4" onClick={handleClickXL}>
+                                            <div style={{fontSize:"18px",fontWeight:"700",pointerEvents:"none"}}>24</div>
+                                            <div style={{fontSize:"10px",fontWeight:"500",pointerEvents:"none"}}>hrs</div>
+                                        </div>
+                                    </div>
+                                
+                            </div>
+                            
+                        </div>
+                        <div className ="rental-booking-package-list">
+                            <div style={{minWidth:"50%",maxWidth:"50%",display:"flex",justifyContent:"center",alignItems:"center",fontSize:"18px",fontWeight:"600px"}}>select package</div>
+                            <div className = "rental-booking-package-selectform">
+                                <FormControl style={{minWidth:"140px",maxWidth:"140px"}}>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        name = "packageselect"
+                                        value={packagesXL}
+                                        onChange={handleClickXL}
+                                    >
+                                        <MenuItem value={"none"}>none</MenuItem>
+                                        <MenuItem value={"3 days"}>3 days</MenuItem>
+                                        <MenuItem value={"7 days"}>7 days</MenuItem>
+                                        <MenuItem value={"12 days"}>12 days</MenuItem>
+                                        <MenuItem value={"1 month"}>1 month</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                        </div>
+                        <div className = "rental-booking-pricing-block">
+                            <div className = "rental-booking-price-tag">
+                                <div style={{minWidth:"50%",maxWidth:"50%",maxHeight:"100%",height:"100%",display:"flex",flexDirection:"row"}}>
+                                    <span className="rental-booking-price-tag-block" style={{minWidth:"20px",fontSize:"26px",fontWeight:"400",justifyContent:"center",alignItems:"center"}}>₹ </span>
+                                    <span className="rental-booking-price-tag-block" style={{fontSize:"28px",fontWeight:"600",
+                                    justifyContent:"center",alignItems:"center"}}>1,000 </span>
+                                    <span className="rental-booking-price-tag-block" style={{fontSize:"12px",fontWeight:"400",alignItems:"flex-end",padding:"7px 3px"}}>/12hr </span>
+                                </div>
+                            
+                                <div style={{minHeight:"16px",maxHeight:"16px",fontSize:"12px",textAlign:"left",paddingLeft:"5px",fontWeight:"400"}}>
+                                    (Inclusive of taxes)
+                                </div>
+                            </div>
+                            <div style={{minHeight:"24px",maxHeight:"24px",fontSize:"15px",textAlign:"left",paddingLeft:"5px",fontWeight:"600"}}>+ 2rs per km</div>
+                            <div></div>
+                        </div>
+                        <div style={{minHeight:"40px",display:"flex",flexDirection:"row",alignItems:"center"}}>
+                            <form autoComplete="off" style={{maxHeight:"30px",paddingLeft:"10px",display:"flex",flexDirection:"row"}} onSubmit={promoCodeSubmit}>
+
+                                <TextField style={{maxHeight:"100%",backgroundColor:"white",}} id="standard-basic" placeholder="apply promocode" value={promocodeValue} onChange = {e => setPromocodeValue(e.target.value)}  />
+                                <div style={{paddingLeft:"20px"}}>
+                                    <button  >apply</button>
+                                </div>
+
+                            </form>
+                        </div>
+                        <div className = "rental-booking-large-farecalculator">
+                            <div className = "rental-booking-fare-breakup-block">
+                                <div className = "rental-booking-fare-breakup-block-left">Base Fare</div>
+                                <div className = "rental-booking-fare-breakup-block-right">Rs. 1200/-</div>
+                            </div>
+                            <div className = "rental-booking-fare-breakup-block">
+                                <div className = "rental-booking-fare-breakup-block-left">Extra charges</div>
+                                <div className = "rental-booking-fare-breakup-block-right">Rs. 120</div>
+                            </div>
+                            <div className = "rental-booking-fare-breakup-block" >
+                                
+                            </div>
+                            <div className = "rental-booking-fare-breakup-block">
+                                <div className = "rental-booking-fare-breakup-block-left">Total Fare</div>
+                                <div className = "rental-booking-fare-breakup-block-right">Rs.1400/-</div>
+                            </div>
+
+                        </div>
+                        <div className = {`rental-booking-confirm-button-small show` }>
+                            <div className="rental-booking-confirm-button-small-block">confirm booking</div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
